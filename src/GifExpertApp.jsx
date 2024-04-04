f6db2a614f21d4410cca9c8f0e9c63fa84fe76d4
AlvaroMata8 <alvaromataaracil@hotmail.com>
2024-04-04 13:52:22 +0200

    AM usestate + button

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Piece', 'One punch'])
+
+    console.log( categories )
+
+    const onAddCategory = () => {
+        setCategories([ ...categories, 'Dragon ball' ])
+        // setCategories(cat => [ ...cat, 'Dragon ball' ])
+    }
+
     return (
         <>
-            <h1>
-                GifExpertApp
-            </h1>
+
+            {/* {title} */}
+            <h1> GifExpertApp </h1>
+
+            {/* {Input} */}
+
+            {/* {Gif List} */}
+            <button onClick={ onAddCategory }>Add</button>
+            <ol>
+                { categories.map( (category) => {
+                    return <li key={ category } >{ category }</li>
+                }) }
+            </ol>
+                {/* {Gif item} */}
+
+
         </>
     );
 }
